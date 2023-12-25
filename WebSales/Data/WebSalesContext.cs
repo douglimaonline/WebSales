b064082ee7f989fb4ee284d50c1d738111e942a2
douglas <douglimaonline@gmail.com>
2023-12-25 17:26:00 -0300

    Second Migration

--- WebSales/Data/WebSalesContext.cs
+++ WebSales/Data/WebSalesContext.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using WebSales.Models;
 
 namespace WebSales.Data
 {
     public class WebSalesContext : DbContext
     {
-        public WebSalesContext (DbContextOptions<WebSalesContext> options)
+        public WebSalesContext(DbContextOptions<WebSalesContext> options)
             : base(options)
         {
         }
 
-        public DbSet<WebSales.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; } = default!;
+        public DbSet<SalesRecord> SalesRecords { get; set; } = default!;
+
     }
 }
--- WebSales/Migrations/20231225201814_OtherEntities.Designer.cs
+++ WebSales/Migrations/20231225201814_OtherEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSales.Data;
 
@@ -10,9 +11,11 @@ using WebSales.Data;
 namespace WebSales.Migrations
 {
     [DbContext(typeof(WebSalesContext))]
-    partial class WebSalesContextModelSnapshot : ModelSnapshot
+    [Migration("20231225201814_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
