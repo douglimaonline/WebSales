4c95502b9fb3b71da1432a9d08c6aa04d880ca4e
douglas <douglimaonline@gmail.com>
2023-12-25 18:51:04 -0300

    Add SellersController

---
+++ WebSales/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSales.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
