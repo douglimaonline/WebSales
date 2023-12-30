92fbf24a28b98148f8d0ec64b05845c029157c1a
douglas <douglimaonline@gmail.com>
2023-12-30 11:24:14 -0300

    Drop Birthdate from Seller Model

--- WebSales/Models/Seller.cs
+++ WebSales/Models/Seller.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
@@ -14,12 +13,11 @@
         {
         }
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        public Seller(int id, string name, string email, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
         }
