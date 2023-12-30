fb93234d91bac6788d1b9634e9dcce38cf44ae67
douglas <douglimaonline@gmail.com>
2023-12-30 11:22:50 -0300

    Drop Birthday from sellers

---
+++ WebSales/Migrations/20231230141003_drop_Birthdate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebSales.Migrations
+{
+    /// <inheritdoc />
+    public partial class drop_Birthdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Seller");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Seller",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
