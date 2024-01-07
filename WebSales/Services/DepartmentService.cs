e0dd05d23c5c79bd55d7199810ac344fcc98e8d1
douglas <douglimaonline@gmail.com>
2024-01-07 14:49:29 -0300

    Add DepartmentServices

---
+++ WebSales/Services/DepartmentService.cs
@@ -0,0 +1,19 @@
+﻿using WebSales.Data;
+using WebSales.Models;
+
+namespace WebSales.Services
+{
+    public class DepartmentService
+    {
+        private readonly WebSalesContext _context;
+
+        public DepartmentService(WebSalesContext context)
+        {
+            _context = context;
+        }
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
